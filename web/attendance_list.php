82925639f17588112b3324cc74edf2f9ac689368
amira bahanni <159464250+Amirabahanni@users.noreply.github.com>
2024-05-17 18:18:43 +0200

    attendance_list.php

--- web/attendance_list.php
+++ web/attendance_list.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "amira";
+$password = "insert password here";
 $dbname = "web";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
